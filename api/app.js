0693a0589400095f8040e3180cf9e040979b565d
GeoffreyKimani <jeffkim207@gmail.com>
2023-07-11 08:07:58 +0200

    app file changed

--- api/app.js
+++ api/app.js
@@ -1,13 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
+
+// Create app
 var app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// specify port to run the app
+const PORT = process.env.PORT || 3000;
 
+// Add parser before specifying the route
+app.use(bodyParser.json());
+
+// specify router entry
+app.use('/', routes)
 routes(app);
 
-var server = app.listen(3000, function () {
+var server = app.listen(PORT, function () {
     console.log("app running on port.", server.address().port);
 });
