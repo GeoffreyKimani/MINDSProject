a01ff55f4859ebeb4577cad9a63840a01476f0cb
GeoffreyKimani <jeffkim207@gmail.com>
2023-07-13 22:16:54 +0200

    read from file in server

--- api/database/db_util.js
+++ api/database/db_util.js
@@ -1,8 +1,6 @@
 const db = require('./data.json');
 
-const getAllCountryLeaders = async (country) => {
-    const response = await fetch('https://raw.githubusercontent.com/GeoffreyKimani/MINDSProject/main/api/database/data.json');
-    const data = await response.json();
+const getAllCountryLeaders = (country) => {
     return db[country];
 } 
 
